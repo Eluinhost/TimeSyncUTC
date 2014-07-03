2718de3a72ea4f713d365e20549e51e843fda69f
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-03 16:20:44 +0100

    fetch time offset async

--- src/main/java/me/malory/plugin/SyncTimeUTC/SyncTimeUTC.java
+++ src/main/java/me/malory/plugin/SyncTimeUTC/SyncTimeUTC.java
@@ -1,7 +1,11 @@
 package me.malory.plugin.SyncTimeUTC;
 
 
-import org.apache.commons.net.ntp.NTPUDPClient;
+import com.comphenix.executors.BukkitExecutors;
+import com.comphenix.executors.BukkitScheduledExecutorService;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import org.apache.commons.net.ntp.TimeInfo;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -9,12 +13,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class SyncTimeUTC extends JavaPlugin {
+
+    private final BukkitScheduledExecutorService async = BukkitExecutors.newAsynchronous(this);
 
     public static final String PREFIX = ChatColor.GRAY + "[" + ChatColor.GOLD + "SyncTimeUTC" + ChatColor.GRAY + "] " + ChatColor.DARK_GREEN;
     public static final SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd HH:mm:ss z");
@@ -32,37 +37,43 @@ public class SyncTimeUTC extends JavaPlugin {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args)
     {
         if (sender.hasPermission("synctimeutc.use")) {
             if (cmd.getName().equalsIgnoreCase("SyncTimeUTC")) {
-                try {
-                    NTPUDPClient client = new NTPUDPClient();
-                    InetAddress address = InetAddress.getByName("time.nist.gov");
-                    TimeInfo info = client.getTime(address);
-                    info.computeDetails();
+                //create a future for fetching the TimeInfo
+                ListenableFuture<TimeInfo> futureTimeInfo = async.submit(new TimeSynchronizer());
 
-                    offset = info.getOffset();
-                    long returnTime = info.getReturnTime();
-                    long serverTime = info.getReturnTime() + offset;
+                //when the time is fetched trigger the callback on the main thread
+                Futures.addCallback(futureTimeInfo, new FutureCallback<TimeInfo>() {
+                    @Override
+                    public void onSuccess(TimeInfo info) {
+                        offset = info.getOffset();
 
-                    Date offsetDate = new Date(info.getReturnTime() + offset);
-                    dateFormat.setTimeZone(defaultTimezone);
-                    String defaultTimeZone = dateFormat.format(offsetDate);
-                    dateFormat.setTimeZone(TimeZone.getDefault());
-                    String localTimeZone = dateFormat.format(offsetDate);
+                        long returnTime = info.getReturnTime();
+                        long serverTime = info.getReturnTime() + offset;
 
-                    sender.sendMessage(PREFIX + "Successful");
-                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Current system time in millis is " + ChatColor.DARK_GREEN + returnTime);
-                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Current time from NIST is " + ChatColor.DARK_GREEN + serverTime);
-                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time (local) is " + ChatColor.DARK_GREEN + localTimeZone);
-                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time is " + ChatColor.DARK_GREEN + defaultTimeZone);
-                    return true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    sender.sendMessage(PREFIX + ChatColor.RED + "Error fetching time data");
-                    return true;
-                }
+                        Date offsetDate = new Date(info.getReturnTime() + offset);
+                        dateFormat.setTimeZone(defaultTimezone);
+                        String defaultTimeZone = dateFormat.format(offsetDate);
+                        dateFormat.setTimeZone(TimeZone.getDefault());
+                        String localTimeZone = dateFormat.format(offsetDate);
+
+                        sender.sendMessage(PREFIX + "Successful");
+                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Current system time in millis is " + ChatColor.DARK_GREEN + returnTime);
+                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Current time from NIST is " + ChatColor.DARK_GREEN + serverTime);
+                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time (local) is " + ChatColor.DARK_GREEN + localTimeZone);
+                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time is " + ChatColor.DARK_GREEN + defaultTimeZone);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        throwable.printStackTrace();
+                        sender.sendMessage(PREFIX + ChatColor.RED + "Error fetching time data");
+                    }
+                });
+
+                return true;
             }
         }
         if (cmd.getName().equalsIgnoreCase("UTC")) {
