f4103200c48b6a51a558b67f2328dbc698e6c9e8
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-03 16:29:42 +0100

    change permission check to inside the command. add permission for utc command. change permission name

--- src/main/java/me/malory/plugin/SyncTimeUTC/SyncTimeUTC.java
+++ src/main/java/me/malory/plugin/SyncTimeUTC/SyncTimeUTC.java
@@ -42,55 +42,68 @@ public class SyncTimeUTC extends JavaPlugin
     @Override
     public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args)
     {
-        if(sender.hasPermission("synctimeutc.use")) {
-            if(cmd.getName().equalsIgnoreCase("SyncTimeUTC")) {
-                //create a future for fetching the TimeInfo
-                ListenableFuture<TimeInfo> futureTimeInfo = async.submit(new TimeSynchronizer(timeserverUrl));
 
-                //when the time is fetched trigger the callback on the main thread
-                Futures.addCallback(futureTimeInfo, new FutureCallback<TimeInfo>()
-                {
-                    @Override
-                    public void onSuccess(TimeInfo info)
-                    {
-                        offset = info.getOffset();
-
-                        long returnTime = info.getReturnTime();
-                        long serverTime = info.getReturnTime() + offset;
-
-                        Date offsetDate = new Date(info.getReturnTime() + offset);
-                        dateFormat.setTimeZone(defaultTimezone);
-                        String defaultTimeZone = dateFormat.format(offsetDate);
-                        dateFormat.setTimeZone(TimeZone.getDefault());
-                        String localTimeZone = dateFormat.format(offsetDate);
-
-                        sender.sendMessage(PREFIX + "Successful");
-                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Current system time in millis is " + ChatColor.DARK_GREEN + returnTime);
-                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Current time from NIST is " + ChatColor.DARK_GREEN + serverTime);
-                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time (local) is " + ChatColor.DARK_GREEN + localTimeZone);
-                        sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time is " + ChatColor.DARK_GREEN + defaultTimeZone);
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable)
-                    {
-                        throwable.printStackTrace();
-                        sender.sendMessage(PREFIX + ChatColor.RED + "Error fetching time data");
-                    }
-                });
-
+        if(cmd.getName().equalsIgnoreCase("SyncTimeUTC")) {
+            if(!sender.hasPermission("synctimeutc.sync")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to run this command");
                 return true;
             }
+
+            //create a future for fetching the TimeInfo
+            ListenableFuture<TimeInfo> futureTimeInfo = async.submit(new TimeSynchronizer(timeserverUrl));
+
+            //when the time is fetched trigger the callback on the main thread
+            Futures.addCallback(futureTimeInfo, new FutureCallback<TimeInfo>()
+            {
+                @Override
+                public void onSuccess(TimeInfo info)
+                {
+                    offset = info.getOffset();
+
+                    long returnTime = info.getReturnTime();
+                    long serverTime = info.getReturnTime() + offset;
+
+                    Date offsetDate = new Date(info.getReturnTime() + offset);
+                    dateFormat.setTimeZone(defaultTimezone);
+                    String defaultTimeZone = dateFormat.format(offsetDate);
+                    dateFormat.setTimeZone(TimeZone.getDefault());
+                    String localTimeZone = dateFormat.format(offsetDate);
+
+                    sender.sendMessage(PREFIX + "Successful");
+                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Current system time in millis is " + ChatColor.DARK_GREEN + returnTime);
+                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Current time from NIST is " + ChatColor.DARK_GREEN + serverTime);
+                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time (local) is " + ChatColor.DARK_GREEN + localTimeZone);
+                    sender.sendMessage(PREFIX + ChatColor.AQUA + "Fixed system time is " + ChatColor.DARK_GREEN + defaultTimeZone);
+                }
+
+                @Override
+                public void onFailure(Throwable throwable)
+                {
+                    throwable.printStackTrace();
+                    sender.sendMessage(PREFIX + ChatColor.RED + "Error fetching time data");
+                }
+            });
+
+            //always return true as this will be reached before the callback
+            return true;
         }
+
         if(cmd.getName().equalsIgnoreCase("UTC")) {
+            if(!sender.hasPermission("synctimeutc.use")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to run this command");
+                return true;
+            }
+
             TimeZone chosenZone = defaultTimezone;
             if(args.length > 0) {
                 //load the timezone from args[0], if invalid supplid it reverts to GMT
                 chosenZone = TimeZone.getTimeZone(args[0]);
             }
+
             Date offsetDate = new Date(System.currentTimeMillis() + offset);
             dateFormat.setTimeZone(chosenZone);
             String offsetString = dateFormat.format(offsetDate);
+
             sender.sendMessage(PREFIX + ChatColor.AQUA + "Currently it is " + ChatColor.DARK_GREEN + offsetString);
             return true;
         }
